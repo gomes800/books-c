e434d5110e4b01db8cee288b411edac917f25b22
gom-trab <jgomesleite8@gmail.com>
2026-02-16 12:50:43 -0300

    read .csv file

--- main.c
+++ main.c
@@ -5,6 +5,35 @@
 
 int main() {
 
+    FILE *f = fopen("data.csv", "r");
+
+    if (!f) {
+        perror("fopen");
+        return 1;
+    }
+
+    char line[256];
+
+    fgets(line, sizeof(line), f);
+
+    while (fgets(line, sizeof(line), f)) {
+        char id[32];
+        char name[64];
+        char author[64];
+        int read;
+
+        if (sscanf(
+                    line,
+                     " %31[^,],%63[^,],%63[^,],%d",
+                     id, name, author, &read
+                  ) == 4) {
+            printf("ID=%s | Name=%s | Author=%s | Read=%d\n",
+                    id, name, author, read);
+        }
+    }
+
+    fclose(f);
+
     Book b1 = {"20216574", "beautiful book", "beautiful writer", true};
 
     addBook(b1);
